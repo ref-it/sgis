0039c72f6739a6fadc4f219da5b98a3a7470ca55
M. Braun <michael-dev@fami-braun.de>
2014-01-05 12:02:03 +0100

    add box-services

---
+++ www/rpcrefresh.php
@@ -0,0 +1,69 @@
+<?php
+
+# wird benutzt, um zu gegebenen Nutzernamen die Gruppen abzufragen sowie die Information, ob er sich noch einloggen darf
+
+require_once "../lib/inc.all.php";
+global $rpcKey2;
+
+if (!isset($_POST["login"])) die ("missing login");
+
+function getData() {
+  global $rpcKey2, $pwObj;
+  $ret = Array();
+
+
+  $login = decrypt((string)$_POST["login"], $rpcKey2);
+  if ($login === false) {
+    $ret["status"] = "err";
+    $ret["msg"] = "bad login string";
+    return $ret;
+  }
+  $login = json_decode($login, true);
+  $ret["nonce"] = $login["nonce"];
+  $ret["replies"] = Array();
+  foreach ($login["username"] as $username) {
+    $ret["replies"][$username] = getSinglePersonData($username);
+  }
+  $ret["status"] = "ok";
+  return $ret;
+}
+
+function getSinglePersonData($username) {
+  $ret = Array();
+  $person = getPersonDetailsByUsername($username);
+  if ($person === false) {
+    $ret["status"] = "badlogin";
+    $ret["msg"]    = "user does not exist";
+    $ret["grps"]   = Array();
+    return $ret;
+  }
+  $ret["person"] = $person;
+  unset($ret["person"]["password"]);
+  $grps = Array();
+  foreach (getPersonGruppe($person["id"]) as $grp) {
+    $grps[] = $grp["name"];
+  }
+  $ret["grps"] = $grps;
+  if ($person["canLogin"]) {
+    $canLogin = !in_array("cannotLogin", $grps);
+  } else {
+    $canLogin = in_array("canLogin", $grps);
+  }
+  $ret["canLogin"] = $canLogin;
+  if (!$canLogin) {
+    $ret["status"] = "badlogin";
+    $ret["msg"]    = "user not permitted to login";
+    return $ret;
+  }
+  $ret["status"] = "oklogin";
+  $ret["msg"]    = "Login permitted";
+  return $ret;
+}
+
+$ret = getData();
+
+header("Content-Type: application/octet-stream");
+echo encrypt(json_encode($ret), $rpcKey2);
+
+exit;
+
