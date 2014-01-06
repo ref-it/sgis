e3d59656f04022f52b0bb0b600669ebae0f36c7c
M. Braun <michael-dev@fami-braun.de>
2014-01-06 19:48:15 +0100

    fix username permitted chars

--- lib/Auth/Source/SGIS.php
+++ lib/Auth/Source/SGIS.php
@@ -48,7 +48,8 @@ class sspmod_sgis_Auth_Source_SGIS extends sspmod_sgis_Auth_UserPassBaseCookie {
 
         // This function receives the username and password the user entered, and is expected to return the attributes of that user. If the username or password is incorrect, it should throw an error saying so.
         function login($username, $password) {
-                # username needs to match ^[a-z][-a-z0-9\._]*\$
+                # username needs to match ^[a-z][-a-z0-9_]*\$
+                # permit dot here as some users still have it
                 $username  = preg_replace('/^[^a-z]*/', '', preg_replace('/[^-a-z0-9\._]/', '', strtolower($username)));
 
                 $pdo = new PDO((string) $this->config["dsn"], (string) $this->config["username"], (string) $this->config["password"]);
