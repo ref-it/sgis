2d0576e2cbd2aaeafd5ea08046b5ac741ef1255b
M. Braun <michael-dev@fami-braun.de>
2016-05-23 17:43:29 +0200

    initial commit

---
+++ action/sgis.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DokuWiki Plugin etherpadlite (Action Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Michael Braun <michael-dev@fami-braun.de>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+require_once DOKU_PLUGIN.'action.php';
+
+class action_plugin_sgis_sgis extends DokuWiki_Action_Plugin {
+
+    private $sgisGroup = "sgis";
+    private $sgisUrl = "http://helfer.stura.tu-ilmenau.de/sgis/";
+
+    public function register(Doku_Event_Handler &$controller) {
+        $controller->register_hook('TEMPLATE_USERTOOLS_DISPLAY', 'BEFORE', $this, 'add_menu_item');
+    }
+
+    public function add_menu_item(Doku_Event $event, $param) {
+        global $ACT, $INFO, $USERINFO, $lang;
+
+        $isSgis = in_array("sgis", $USERINFO['grps']);
+
+        if (!$isSgis)
+          return;
+
+        $event->data['items']['sgis'] = "<li>".tpl_link($this->sgisUrl,$lang["btn_profile"].' (sGIS)','',true)."</li>";
+    }
+}
+
+// vim:ts=4:sw=4:et:
