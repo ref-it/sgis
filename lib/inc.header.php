8cadc017a2070f61887fd1804f7e8e1a20c583a7
M. Braun <michael-dev@fami-braun.de>
2017-06-16 17:58:34 +0200

    avoid browser caching

--- lib/inc.header.php
+++ lib/inc.header.php
@@ -5,3 +5,10 @@
 header("X-Content-Security-Policy: allow 'self'; options inline-script eval-script");
 header("X-Frame-Options: DENY");
 
+// do not cache me
+header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
